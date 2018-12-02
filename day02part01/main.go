7f9e5758859bf6567f213bed43e4f864a3dca7bc
Sam Lewis <mazuel@gmail.com>
2018-12-03 00:30:44 +1030

    add day02

---
+++ day02part01/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func count(in string) (bool, bool) {
+	count := make(map[rune]int)
+	has2 := false
+	has3 := false
+
+	for _, char := range in {
+		count[char]++
+	}
+
+	for _, val := range count {
+		if val == 2 {
+			has2 = true
+		}
+
+		if val == 3 {
+			has3 = true
+		}
+	}
+
+	return has2, has3
+}
+
+func solution(input []string) int {
+	var count2 int
+	var count3 int
+
+	for _, val := range input {
+		h2, h3 := count(val)
+		if h2 {
+			count2++
+		}
+
+		if h3 {
+			count3++
+		}
+		fmt.Println(val, h2, h3)
+	}
+
+	return count2 * count3
+}
+
+func readFromStdin() []string {
+	var res []string
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		// fmt.Println(scanner.Text())
+
+		// String
+		res = append(res, scanner.Text())
+	}
+
+	return res
+}
+
+func main() {
+	input := readFromStdin()
+	answer := solution(input)
+
+	fmt.Println("ANSWER: ", answer)
+}
---
+++ day02part02/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func diff(lhs string, rhs string) int {
+	d := byte('.')
+	pos := -1
+	for i := 0; i < len(lhs); i++ {
+		if lhs[i] != rhs[i] {
+			if d != '.' {
+				return -1
+			}
+			d = lhs[i]
+			pos = i
+		}
+	}
+
+	return pos
+}
+
+func solution(input []string) string {
+	for i, val := range input {
+		for k := i + 1; k < len(input); k++ {
+			pos := diff(input[i], input[k])
+			if pos != -1 {
+				return (val[0:pos] + val[pos+1:])
+			}
+		}
+	}
+
+	return "NOPE"
+}
+
+func readFromStdin() []string {
+	var res []string
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		res = append(res, scanner.Text())
+	}
+
+	return res
+}
+
+func main() {
+	input := readFromStdin()
+	answer := solution(input)
+
+	fmt.Println("ANSWER: ", answer)
+}
